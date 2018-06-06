af516a83b9babb486dbd0e4a7ab4e32992809d7d
kolaveridi <coderboysatya@gmail.com>
2018-06-06 23:09:00 +0530

    react router and indexrouter

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+import App from './App';
+import PostsIndex from './components/posts_index';
+const Greeting =()=>{
+  return <div> Hey Greeting </div>;
+};
+export default(
+   <Route path="/" component={App}>
+   <IndexRoute  component={PostsIndex}/>
+   <Route path="greet" component={Greeting}/>
+   </Route>
+);
+//// Note when we we do simple http://localhost:3000/
+///we get both app and postsindex rendered on screen by default
+//// note when do http://localhost:3000/greet we get both App and greet rendered on the screen
